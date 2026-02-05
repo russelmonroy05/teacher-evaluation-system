c55f0c12157c32ec7ca5668f5cc0a244688d329b
russelmonroy05 <157871041+russelmonroy05@users.noreply.github.com>
2026-02-05 23:55:24 +0800

    Fix PostgreSQL engine for Django with psycopg3

--- CCSTPES/teacher_eval_project/teacher_eval_project/settings.py
+++ CCSTPES/teacher_eval_project/teacher_eval_project/settings.py
@@ -137,8 +137,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='bernieromero643@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='gboskhemdnikonfg')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=f'Teacher Evaluation <{EMAIL_HOST_USER}>')
 
 # -----------------------------
